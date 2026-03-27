d69400d22b32eb6d4424e6d7b7257d2d9476f24a
carilise <floredoko67@gmail.com>
2026-03-27 18:06:41 +0100

    Fix animations avec framer-motion

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "./",
   preview: {
     allowedHosts: ["portofoliokousseylarissa-2.onrender.com"],
     port: process.env.PORT || 4173,
